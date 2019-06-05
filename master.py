25be465cee370a1cdaff88496f8c417d82235928
noahbetik <51169718+noahbetik@users.noreply.github.com>
2019-06-05 13:42:09 -0400

    Add files via upload
    Added serial communications framework; need to find actual port for receiving data from RasPi. Currently only supports static target (i.e. will not follow a moving target). Also only receives a single value for "errorAngle" -- need to determine format and then run string splitting.
    
    Integration of the serial comms should probably be improved in order to possibly support live target tracking and for all the code to be in its own nice little file.

--- master.py
+++ master.py
@@ -4,17 +4,29 @@ from ev3dev2.motor import *
 from time import sleep
 from PID import PID
 from trigger import buttonBop
+import serial
 
-targetState = True
-#errorAngle = int(input())   prototyping code for now, will actually pull offset angle from vision tracking
+targetState = False
+errorAngle = 0   #prototyping code for now, will actually pull offset angle from vision tracking
+port = "\\\\.\\CNCA0" #example port, update once found
+ser = serial.Serial(port, 38400, timeout=0)
 
 spinAction = PID(0.3, 0.25, 0, 0, 0, LargeMotor(OUTPUT_A))
 liftAction = PID(0.3, 0.25, 0, 0, 0, LargeMotor(OUTPUT_B))
+
+def receive():
+    data = ser.read(9999)
+    if len(data) > 0:
+        errorAngle = data
+        print (errorAngle)
+    else:
+        print ("no target received")
 
 while True:
 
     if (targetState == False):
         spinAction.search()
+        receive()
 
     elif (targetState == True):
         spinAction.pos()
@@ -25,16 +37,13 @@ while True:
 
     else:
         spinAction.worldHasEnded()
+        ser.close
 
 
 
 
 # need some vision/motion integration to change targetState
-# also need to provide spinAction and liftAction with setPoint values
-# setPoint may need to be calculated from an angle offset given by vision
-# thread-splitting to run spinAction and liftAction at the same time???
-
-#yippity yote, get off of my goat
+# currently can only move to static target, doesn't live-check while running PID loop
 
 
 
